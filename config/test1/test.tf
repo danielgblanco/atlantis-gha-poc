0c5e2724112ed022071e06b5890f798ecf90269b
Daniel Gomez Blanco <mail@dangb.me>
2023-03-16 19:31:44 +0000

    Update test.tf

--- config/test1/test.tf
+++ config/test1/test.tf
@@ -1,5 +1,5 @@
 data "http" "example" {
-  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+  url = "https://checkpoint-api.hashicorp.com/v1/check/terraformtest"
 
   # Optional request headers
   request_headers = {
